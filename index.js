a0661142618f678498b1de2b682888b4f92a0b2f
Ramildo82 <53921093+Ramildo82@users.noreply.github.com>
2021-04-03 00:42:19 -0300

    parte 3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const path = require('path');
+const config = require('config');
+
+const port = process.env.PORT || config.get('server.port');
+
+const  lancamentos = [
+    { data: '2021-03-10,',
+    descricao:'pagamento',
+    valor: -100.00
+    },
+    { data: '2021-02-06,',
+    descricao:'pagamento',
+    valor: -20.00
+    },
+    { data: '2021-05-24,',
+    descricao:'pagamento',
+    valor: -500.00
+    },
+]
+
+const app = express();
+
+app.set('port', port);
+
+app.route('/extrato').get(
+    (req, res)=> {
+        res.status(200).json(lancamentos)
+    }
+)
+
+app.listen(port, ()=> {
+    console.log("Servidor iniciado na porta " + port);
+})
+
 console.log("serviço iniciado")
